6a072cd77cd84785d23c4c60148845627501cf4d
CristianRojas6 <https://github.com/CristianRojas6>
2020-02-05 20:55:51 -0500

    Mejoramiento del header final

--- js/javascript.js
+++ js/javascript.js
@@ -1,49 +1,106 @@
 /* Esta funcion busca una pelicula y muestra el logo de esta*/
 
 
-function buscarPelicula() {
+/* Esta funcion busca una pelicula y muestra el logo de esta*/
+function buscarPelicula(){	
+	swal({
+	  title:"Bienvenido a StarFilms",
+	  content: "input",
+	  dangerMode: {	
+	  text: "Search!",
+	  closeModal: false,
+	  },
+	})
+	.then(name => {
+	  if (!name) throw null;
+	 
+	  return fetch(`https://itunes.apple.com/search?term=${name}&entity=movie`);
+	})
+	.then(results => {
+	  return results.json();
+	})
+	.then(json => {
+	  const movie = json.results[0];
+	 
+	  if (!movie) {
+	    return swal("Actualmente no esta en cartelera!");
+	  }
+	 
+	  const name = movie.trackName;
+	  const imageURL = movie.artworkUrl100;
+	 
+	  swal({
+	    title: "StarFilms",
+	    text: name,
+	    icon: imageURL,
+	  });
+	})
+	.catch(err => {
+	  if (err) {
+	    swal("Oh noes!", "The AJAX request failed!", "error");
+	  } else {
+	    swal.stopLoading();
+	    swal.close();
+	  }
+	});
+	}
 
-    swal({
-            text: 'Busca tu película:',
-            content: "input",
-            button: {
-                text: "Search!",
-                closeModal: false,
-            },
-        })
-        .then(name => {
-            if (!name) throw null;
+	/* este codigo es para evaluar el servicio que se presta a nuestros usuarios  */
 
-            return fetch(`https://itunes.apple.com/search?term=${name}&entity=movie`);
-        })
-        .then(results => {
-            return results.json();
-        })
-        .then(json => {
-            const movie = json.results[0];
-
-            if (!movie) {
-                return swal("Actualmente no está en cartelera!");
-            }
-
-            const name = movie.trackName;
-            const imageURL = movie.artworkUrl100;
-
-            swal({
-                title: "StarFilms",
-                text: name,
-                icon: imageURL,
-            });
-        })
-        .catch(err => {
-            if (err) {
-                swal("Oh noes!", "The AJAX request failed!", "error");
-            } else {
-                swal.stopLoading();
-                swal.close();
-            }
-        });
+function servicio(){
+	swal({
+		title: "Como estuvo el servicio y la atencion del cine",
+		buttons: {
+		cancel: "pesimo",
+		catch: {
+		   text: "normal",
+		   value: "normal",
+		 },
+	    excelente: true,
+		  },
+		})
+		.then((value) => {
+		  switch (value) {
+		 
+		    case "excelente":
+		      swal("nuestros clientes son lo mejor de nosotros","seguiremos mejorando","../../bictia/encabezado/img/feliz.png");
+		      break;
+		 
+		    case "normal":
+		      swal("ayudanos a mejor", " dando tu opinion en sugerencias", "../../bictia/encabezado/img/carita-normal.png");
+		      break;
+		 
+		    default:
+		      swal("Sentimos las molestias","Porfavor llene el formulario de quejas","../../bictia/encabezado/img/carita-reclamo.jpeg");
+		  }
+		});
 }
+
+
+function ingresar(Nombre, Password){
+	var nombreUsuario = document.getElementById('formGroupExampleInput').value;
+	var usuarioPassword = document.getElementById('formGroupExampleInput2').value;
+	localStorage.setItem(nombreUsuario, usuarioPassword);
+	if(usuarioPassword == "bictia" && localStorage.length != null){
+		document.getElementById('nombreUsuarioCine').innerHTML = nombreUsuario;
+		document.getElementById('cerrarSecionCine').style.display = 'block';
+		document.getElementById('esconderIngresar').style.display = 'none'; 
+		document.getElementById('ingresoFormulario1').style.backgroundColor = "black";
+
+	} else{
+		document.getElementById('nombreUsuarioCine').innerHTML = "try again";
+	}
+}
+
+function cerrarSecion(){
+	var nombreUsuario = document.getElementById('formGroupExampleInput').value;
+	var usuarioPassword = document.getElementById('formGroupExampleInput2').value;
+	localStorage.removeItem(nombreUsuario, usuarioPassword);
+	document.getElementById('nombreUsuarioCine').innerHTML = "StarFilms";
+	document.getElementById('cerrarSecionCine').style.display = 'none';
+	document.getElementById('esconderIngresar').style.display = 'block'; 
+	swal("Espero que tu estadia haya sido increible ", "espero tu regreso", "../../bictia/encabezado/img/logo.png");
+}  
 
 /* este codigo es para evaluar el servicio que se presta a nuestros usuarios  */
 
